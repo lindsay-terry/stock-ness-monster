bf63a839a4839f27ff6315147678bf96651173b7
Lindsay Terry <lindsaytee66@gmail.com>
2024-07-18 22:34:03 -0600

    Fills out models and associations

---
+++ models/Customer.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Customer extends Model {}
+
+Customer.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        company_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        account_manager_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        }, 
+    },
+);
+
+module.exports = Customer;
--- server.js
+++ server.js
@@ -30,14 +30,14 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
