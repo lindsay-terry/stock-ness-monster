21bb4961db2d10a9db7507415930c7d7ad248953
Lindsay Terry <lindsaytee66@gmail.com>
2024-07-20 19:34:51 -0600

    Adds api routes to api/index.js for customer and product

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const productRoutes = require('./productRoutes');
+const customerRoutes = require('./customerRoutes');
 
 router.use('/users', userRoutes);
+router.use('/products', productRoutes);
+router.use('/customers', customerRoutes);
 
 module.exports = router;
