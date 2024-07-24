cd3496d2195ef3e9c7e9449e5d08192d77cfac8e
Lindsay Terry <lindsaytee66@gmail.com>
2024-07-24 17:51:30 -0600

    Replaces existing customer view with new and modifies client side js and server side customer route

--- controllers/api/customerRoutes.js
+++ controllers/api/customerRoutes.js
@@ -4,27 +4,16 @@ const { Customer, User, Order, Product } = require('../../models');
 //retrieve specific customer data by id and associated user and order information
 router.get('/:id', async (req, res) => {
     try {
-        const customerData = await Customer.findByPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: [
-                        'id', 'first_name', 'last_name',
-                    ],
-                },
-                {
-                    model: Order,
-                    attributes: [
-                        'id', 'isFulfilled', 'content',
-                    ],
-                },
-            ],
+        const data = await Order.findAll({
+            include: [{ model: Product }],
+            where: {
+                customer_id: req.params.id,
+            },
         });
-        const customer = customerData.get({ plain: true });
-        res.status(200).json(customer);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+        const orderData = data.map(order => order.get({ plain: true }));
+        res.status(200).json(orderData);
+    } catch (error) {
+        res.status(500).json(error);
     }
 });
 
