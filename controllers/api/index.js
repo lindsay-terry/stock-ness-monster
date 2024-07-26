b997a651bc9221d7a2c61c3440fc9b6900fafee5
Olivier Ndicunguye <nolivanoly@gmail.com>
2024-07-25 18:47:30 -0700

    Created the product availability, and making order function

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
 const customerRoutes = require('./customerRoutes');
+const orderRoutes = require('./orderRoutes');
+const availabilityRoutes = require('./availabilityRoutes');
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/customers', customerRoutes);
+router.use('/orders', orderRoutes);
+router.use('/availability', availabilityRoutes);
 
 module.exports = router;
